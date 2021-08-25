431d6123b5a143d8366593fa8662bead47dd01ad
Izadi Egizabal <ts-izadi.egizabal@rakuten.com>
2021-08-26 02:01:27 +0900

    refactor: decouple network check from repo to interceptor
    fix

--- app/src/main/java/xyz/izadi/aldatu/di/ApiModule.kt
+++ app/src/main/java/xyz/izadi/aldatu/di/ApiModule.kt
@@ -1,27 +1,45 @@
 package xyz.izadi.aldatu.di
 
+import android.content.Context
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okio.IOException
 import retrofit2.Converter
 import retrofit2.Retrofit
 import xyz.izadi.aldatu.data.remote.CurrencyApi
+import xyz.izadi.aldatu.domain.repository.NoInternetConnectionException
 import xyz.izadi.aldatu.utils.Constants
+import xyz.izadi.aldatu.utils.isNetworkAvailable
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class ApiModule {
 
+    companion object {
+        @Qualifier
+        @Retention(AnnotationRetention.RUNTIME)
+        annotation class ApiKeyInterceptor
+
+        @Qualifier
+        @Retention(AnnotationRetention.RUNTIME)
+        annotation class NetworkConnectionInterceptor
+    }
+
     @Singleton
     @Provides
+    @ApiKeyInterceptor
     fun providesApiKeyInterceptor() = Interceptor {
         val url = it.request()
             .url
@@ -37,10 +55,27 @@ class ApiModule {
 
     @Singleton
     @Provides
-    fun providesOkHttpClient(interceptor: Interceptor): OkHttpClient =
+    @NetworkConnectionInterceptor
+    fun providesNetworkConnectionInterceptor(@ApplicationContext appContext: Context) =
+        Interceptor {
+            if (!appContext.isNetworkAvailable()) {
+                throw IOException(NoInternetConnectionException())
+            }
+
+            val builder: Request.Builder = it.request().newBuilder()
+            it.proceed(builder.build())
+        }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(
+        @ApiKeyInterceptor apiKeyInterceptor: Interceptor,
+        @NetworkConnectionInterceptor networkInterceptor: Interceptor
+    ): OkHttpClient =
         OkHttpClient
             .Builder()
-            .addInterceptor(interceptor)
+            .addInterceptor(networkInterceptor)
+            .addInterceptor(apiKeyInterceptor)
             .build()
 
     @ExperimentalSerializationApi

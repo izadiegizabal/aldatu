4ed83f6f2215223dc02fd8323b826d1314eb9568
Izadi Egizabal <ts-izadi.egizabal@rakuten.com>
2021-08-26 22:41:04 +0900

    test: added get rates tests

--- app/src/test/java/xyz/izadi/aldatu/data/repository/CurrencyRepositoryImplTest.kt
+++ app/src/test/java/xyz/izadi/aldatu/data/repository/CurrencyRepositoryImplTest.kt
@@ -13,16 +13,22 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.anyList
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Response
 import xyz.izadi.aldatu.data.local.Currency
 import xyz.izadi.aldatu.data.local.CurrencyListDao
+import xyz.izadi.aldatu.data.local.CurrencyRate
 import xyz.izadi.aldatu.data.local.CurrencyRatesDao
 import xyz.izadi.aldatu.data.local.PreferencesManager
 import xyz.izadi.aldatu.data.remote.CurrencyApi
 import xyz.izadi.aldatu.data.remote.CurrencyListResponse
+import xyz.izadi.aldatu.data.remote.CurrencyRatesResponse
 import xyz.izadi.aldatu.domain.repository.CurrencyRepository
 import xyz.izadi.aldatu.domain.repository.ErrorWhileFetchingException
 import xyz.izadi.aldatu.domain.repository.NoInternetConnectionException
@@ -80,6 +86,9 @@ class CurrencyRepositoryImplTest : TestCase() {
         val actual = sut.getCurrencies()
 
         // assert
+        verify(currencyApi, times(0)).getSupportedCountries()
+        verify(currencyListDao, times(0)).saveCurrencies(anyList())
+        verify(currencyListDao, times(1)).loadCurrencyList()
         assertEquals(mockCurrencyList, actual)
     }
 
@@ -96,14 +105,15 @@ class CurrencyRepositoryImplTest : TestCase() {
         }
         val mockResponse = Response.success(mockCurrencyListResponse)
         `when`(currencyListDao.doWeHaveCurrencies()).thenReturn(false)
-        runCatching {
-            `when`(currencyApi.getSupportedCountries()).thenReturn(mockResponse)
-        }
+        `when`(currencyApi.getSupportedCountries()).thenReturn(mockResponse)
 
         // act
         val actual = sut.getCurrencies()
 
         // assert
+        verify(currencyApi, times(1)).getSupportedCountries()
+        verify(currencyListDao, times(1)).saveCurrencies(anyList())
+        verify(currencyListDao, times(0)).loadCurrencyList()
         assertEquals(actual.size, mockCurrencyList.size)
         actual.forEachIndexed { index, currency ->
             assertThat(currency, samePropertyValuesAs(mockCurrencyList[index]))
@@ -116,9 +126,7 @@ class CurrencyRepositoryImplTest : TestCase() {
             // arrange
             val mockError = NoInternetConnectionException()
             `when`(currencyListDao.doWeHaveCurrencies()).thenReturn(false)
-            runCatching {
-                `when`(currencyApi.getSupportedCountries()).thenThrow(IOException(mockError))
-            }
+            `when`(currencyApi.getSupportedCountries()).thenThrow(IOException(mockError))
 
             // act
             var actual: Exception? = null
@@ -129,6 +137,9 @@ class CurrencyRepositoryImplTest : TestCase() {
             }
 
             // assert
+            verify(currencyApi, times(1)).getSupportedCountries()
+            verify(currencyListDao, times(0)).saveCurrencies(anyList())
+            verify(currencyListDao, times(0)).loadCurrencyList()
             assertEquals(mockError, actual)
         }
 
@@ -139,9 +150,7 @@ class CurrencyRepositoryImplTest : TestCase() {
 
             val mockRes = Response.error<CurrencyListResponse>(401, "Error".toResponseBody())
             `when`(currencyListDao.doWeHaveCurrencies()).thenReturn(false)
-            runCatching {
-                `when`(currencyApi.getSupportedCountries()).thenReturn(mockRes)
-            }
+            `when`(currencyApi.getSupportedCountries()).thenReturn(mockRes)
 
             // act
             var actual: Exception? = null
@@ -152,7 +161,171 @@ class CurrencyRepositoryImplTest : TestCase() {
             }
 
             // assert
+            verify(currencyApi, times(1)).getSupportedCountries()
+            verify(currencyListDao, times(0)).saveCurrencies(anyList())
+            verify(currencyListDao, times(0)).loadCurrencyList()
             assertEquals(mockRes.message(), actual!!.message)
             assertEquals(mockRes.code(), (actual as ErrorWhileFetchingException).errorCode)
         }
+
+
+    @Test
+    fun fetch_currency_rates_local() = coroutineTestRule.testDispatcher.runBlockingTest {
+        // arrange
+        val mockCurrencyRatesList = listOf(
+            CurrencyRate("USDUSD", 1.0, 123L),
+            CurrencyRate("USDEUR", 1.1, 123L)
+        )
+        `when`(prefManager.shouldRefreshRates()).thenReturn(false)
+        `when`(currencyRatesDao.doWeHaveRates()).thenReturn(true)
+        `when`(currencyRatesDao.loadCurrencyRates()).thenReturn(mockCurrencyRatesList)
+
+        // act
+        val actual = sut.getExchangeRates()
+
+        // assert
+        verify(currencyApi, times(0)).getCurrencyRates()
+        verify(currencyRatesDao, times(0)).saveCurrencyRates(anyList())
+        verify(prefManager, times(0)).saveRefreshDate()
+        verify(currencyRatesDao, times(1)).loadCurrencyRates()
+        assertEquals(mockCurrencyRatesList, actual)
+    }
+
+    @Test
+    fun force_refresh_fetches_rates_from_remote() =
+        coroutineTestRule.testDispatcher.runBlockingTest {
+            // arrange
+            `when`(currencyApi.getCurrencyRates()).thenReturn(
+                Response.success(CurrencyRatesResponse())
+            )
+
+            // act
+            sut.getExchangeRates(true)
+
+            // assert
+            verify(currencyApi, times(1)).getCurrencyRates()
+            verify(currencyRatesDao, times(1)).saveCurrencyRates(anyList())
+            verify(prefManager, times(1)).saveRefreshDate()
+            verify(currencyRatesDao, times(0)).loadCurrencyRates()
+        }
+
+    @Test
+    fun preference_should_fetches_rates_from_remote() =
+        coroutineTestRule.testDispatcher.runBlockingTest {
+            // arrange
+            `when`(prefManager.shouldRefreshRates()).thenReturn(true)
+            `when`(currencyApi.getCurrencyRates()).thenReturn(
+                Response.success(CurrencyRatesResponse())
+            )
+
+            // act
+            sut.getExchangeRates()
+
+            // assert
+            verify(currencyApi, times(1)).getCurrencyRates()
+            verify(currencyRatesDao, times(1)).saveCurrencyRates(anyList())
+            verify(prefManager, times(1)).saveRefreshDate()
+            verify(currencyRatesDao, times(0)).loadCurrencyRates()
+        }
+
+    @Test
+    fun no_local_fetches_rates_from_remote() =
+        coroutineTestRule.testDispatcher.runBlockingTest {
+            // arrange
+            `when`(currencyRatesDao.doWeHaveRates()).thenReturn(false)
+            `when`(currencyApi.getCurrencyRates()).thenReturn(
+                Response.success(CurrencyRatesResponse())
+            )
+
+            // act
+            sut.getExchangeRates()
+
+            // assert
+            verify(currencyApi, times(1)).getCurrencyRates()
+            verify(currencyRatesDao, times(1)).saveCurrencyRates(anyList())
+            verify(prefManager, times(1)).saveRefreshDate()
+            verify(currencyRatesDao, times(0)).loadCurrencyRates()
+        }
+
+    @Test
+    fun fetch_currency_rates_remote_success() = coroutineTestRule.testDispatcher.runBlockingTest {
+        // arrange
+        val mockCurrencyRatesList = listOf(
+            CurrencyRate("USDUSD", 1.0, 123L),
+            CurrencyRate("USDEUR", 1.1, 123L)
+        )
+        val mockCurrencyListResponse = CurrencyRatesResponse().apply {
+            rates = mockCurrencyRatesList.map { it.currencyCode to it.rate }.toMap()
+        }
+        val mockResponse = Response.success(mockCurrencyListResponse)
+        `when`(prefManager.shouldRefreshRates()).thenReturn(true)
+        `when`(currencyApi.getCurrencyRates()).thenReturn(mockResponse)
+
+
+        // act
+        val actual = sut.getExchangeRates()
+
+        // assert
+        verify(currencyApi, times(1)).getCurrencyRates()
+        verify(currencyRatesDao, times(1)).saveCurrencyRates(Mockito.anyList())
+        verify(prefManager, times(1)).saveRefreshDate()
+        verify(currencyRatesDao, times(0)).loadCurrencyRates()
+        assertEquals(mockCurrencyRatesList.size, actual.size)
+        actual.forEachIndexed { index, rate ->
+            assertThat(rate, samePropertyValuesAs(mockCurrencyRatesList[index], "timestamp"))
+        }
+    }
+
+    @Test
+    fun fetch_currency_rates_remote_no_internet() =
+        coroutineTestRule.testDispatcher.runBlockingTest {
+            // arrange
+            val mockError = NoInternetConnectionException()
+            `when`(prefManager.shouldRefreshRates()).thenReturn(true)
+            `when`(currencyApi.getCurrencyRates()).thenThrow(IOException(mockError))
+
+
+            // act
+            var actual: Exception? = null
+            runCatching {
+                sut.getExchangeRates()
+            }.onFailure {
+                actual = it as Exception?
+            }
+
+            // assert
+            verify(currencyApi, times(1)).getCurrencyRates()
+            verify(currencyRatesDao, times(0)).saveCurrencyRates(Mockito.anyList())
+            verify(prefManager, times(0)).saveRefreshDate()
+            verify(currencyRatesDao, times(0)).loadCurrencyRates()
+            assertEquals(mockError, actual)
+        }
+
+    @Test
+    fun fetch_currency_rates_remote_error_return() =
+        coroutineTestRule.testDispatcher.runBlockingTest {
+            // arrange
+            val mockRes = Response.error<CurrencyRatesResponse>(401, "Error".toResponseBody())
+            `when`(prefManager.shouldRefreshRates()).thenReturn(true)
+            `when`(currencyApi.getCurrencyRates()).thenReturn(mockRes)
+
+
+            // act
+            var actual: Exception? = null
+            runCatching {
+                sut.getExchangeRates()
+            }.onFailure {
+                actual = it as Exception?
+            }
+
+            // assert
+            verify(currencyApi, times(1)).getCurrencyRates()
+            verify(currencyRatesDao, times(0)).saveCurrencyRates(Mockito.anyList())
+            verify(prefManager, times(0)).saveRefreshDate()
+            verify(currencyRatesDao, times(0)).loadCurrencyRates()
+            assertEquals(mockRes.message(), actual!!.message)
+            assertEquals(mockRes.code(), (actual as ErrorWhileFetchingException).errorCode)
+        }
+
+
 }
